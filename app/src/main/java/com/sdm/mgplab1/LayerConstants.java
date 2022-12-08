e91b68fcb0199babdda9ba7b404c7da006758cea
irfansamad2002 <88933431+irfansamad2002@users.noreply.github.com>
2022-12-08 16:08:11 +0800

    Update LayerConstants.java
    add star_layer = 3

--- app/src/main/java/com/sdm/mgplab1/LayerConstants.java
+++ app/src/main/java/com/sdm/mgplab1/LayerConstants.java
@@ -9,6 +9,7 @@ public class LayerConstants {
     public final static int BACKGROUND_LAYER = 0;
     public final static int RENDERTEXT_LAYER = 1;
     public final static int RENDERSMURF_LAYER = 2;  // New as of 1/12/2022 Week 7
+    public final static int STAR_LAYER = 3;
     public final static int GAMEOBJECTS_LAYER = 100;  // Example
     public final static int UI_LAYER = 1000;
 }
